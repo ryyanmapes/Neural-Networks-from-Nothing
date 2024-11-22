9e2aff9a1676e5b6a71a8e5e7c8e070de1303d5d
Goldey2 <ryyanmapes@gmail.com>
2024-11-22 16:44:23 +0100

    Convolutional Neural Networks!

--- advanced_autodiff/experiments/libraries/autodiff.py
+++ advanced_autodiff/experiments/libraries/autodiff.py
@@ -12,6 +12,8 @@ from enum import Enum
 from typing import Type
 from typing import Tuple
 from collections import ChainMap
+import scipy.sparse as sp
+from numpy.lib.stride_tricks import as_strided
 
 arrayf = Type[np.ndarray[float]]
 
@@ -35,11 +37,14 @@ class Tape():
     topological_order = None
     reverse_topological_order = None
 
+    indices = []
+
     def __init__(self, inputs, output):
         self.input_nodes = inputs
         self.output_node = output
 
         self.input_parameter_count = sum(input_node.variable_count for input_node in self.input_nodes.values())
+        self.indices = [None for _ in range(output.get_output_length())]
     
     def Const(value):
         const_node = Const(value)
@@ -54,6 +59,13 @@ class Tape():
         new_inputs = dict(ChainMap(*[tape.input_nodes for tape in tapes]))
         new_output = Combine([tape.output_node for tape in tapes])
         return Tape(new_inputs, new_output)
+    
+    # todo this is inefficient
+    def sum_all_variables(self, f = lambda x: x):
+        acc = Tape.Const(np.array([[0]]))
+        for input_node in self.input_nodes.values():
+            acc = acc + f(Tape({input_node.input_start_index: input_node}, input_node)).sum()
+        return acc
 
     def __generate_topo_order_if_necessary(self):
         if self.topological_order == None:
@@ -129,6 +141,7 @@ class Tape():
         plt.legend(['$\|f(x) - T_0(x)\|$', '$\|f(x)-T_1(x)\|$'], fontsize=15)
         plt.xticks(fontsize=15)
         plt.yticks(fontsize=15)
+        plt.show()
 
     def scale(self, scalar: float):
         new_output = Scale(scalar, self.output_node)
@@ -154,6 +167,10 @@ class Tape():
         new_output = Div(self.output_node, other.output_node)
         return Tape(new_inputs, new_output)
     
+    def __pow__(self, exponent: float):
+        new_output = Pow(self.output_node, exponent)
+        return Tape(self.input_nodes, new_output)
+    
     def tanh(self):
         new_output = Tanh(self.output_node)
         return Tape(self.input_nodes, new_output)
@@ -172,8 +189,13 @@ class Tape():
         return Tape(new_inputs, new_output)
     
     def __getitem__(self, index):
-        new_output = Index(self.output_node, index)
-        return Tape(self.input_nodes, new_output)
+        if (self.indices[index] == None):
+            new_output = Index(self.output_node, index)
+            new_tape = Tape(self.input_nodes, new_output)
+            self.indices[index] = new_tape
+            return new_tape
+        else:
+            return self.indices[index]
     
     def split(self):
         output_dims = self.output_node.get_output_dimensions()
@@ -181,11 +203,22 @@ class Tape():
         return [self[i] for i in range(output_dims[0])]
 
     def square(self):
-        new_output = Mult(self.output_node, self.output_node)
-        return Tape(self.input_nodes, new_output)
+        return self**2
+    
+    def sqrt(self):
+        return self**0.5
 
     def sum(self):
         new_output = Sum(self.output_node)
+        return Tape(self.input_nodes, new_output)
+    
+    def convolve(self, stencil, strides: Tuple[int, int]):
+        new_inputs = union(self.input_nodes, stencil.input_nodes)
+        new_output = Convolve(self.output_node, stencil.output_node, strides)
+        return Tape(new_inputs, new_output)
+    
+    def reshape(self, new_size):
+        new_output = Reshape(self.output_node, new_size)
         return Tape(self.input_nodes, new_output)
     
     def __str__(self) -> str:
@@ -261,8 +294,11 @@ class AbstractOperationNode(ABC):
         dims = self.get_output_dimensions()
         return dims[0] * dims[1]
     
-    def get_result_matrix(self) -> arrayf: # returns a vector of length output length
-        return self.result.reshape(self.get_output_dimensions())
+    def get_result_matrix(self) -> arrayf:
+        return self.result.reshape(self.get_output_dimensions()) # should never copy!
+    
+    def get_grad_matrix(self) -> arrayf: 
+        return self.grad.reshape(self.get_output_dimensions()) # should never copy!
 
     @abstractmethod
     def __str__(self) -> str:
@@ -455,6 +491,26 @@ class Relu(AbstractOperationNode):
     def __str__(self) -> str:
         return "relu(" + str(self.sources[0]) + ")"
 
+class Pow(AbstractOperationNode):
+
+    exponent: float
+
+    def __init__(self, a: AbstractOperationNode, exponent: float):
+        super().__init__(a)
+        self.exponent = exponent
+
+    def inner_run(self, inputs) -> arrayf: # returns a vector of length output length
+        return np.power(self.sources[0].result, self.exponent)
+    
+    def propagate_grad(self): # assume that grad is completely setup
+        self.sources[0].grad += self.exponent * np.power(self.sources[0].result, self.exponent - 1) * self.grad
+    
+    def get_output_dimensions(self) -> Tuple[int, int]:
+        return self.sources[0].get_output_dimensions()
+
+    def __str__(self) -> str:
+        return "(" + str(self.sources[0]) + ")**" + str(self.exponent)
+
 # matrix-vector multiplication
 class MVMult(AbstractOperationNode):
 
@@ -466,7 +522,7 @@ class MVMult(AbstractOperationNode):
         return (self.sources[0].get_result_matrix() @ self.sources[1].get_result_matrix()).flatten()
 
     def propagate_grad(self): # assume that grad is completely setup
-        self.sources[0].grad += np.outer(self.grad, self.sources[1].result).flatten()
+        self.sources[0].grad += np.outer(self.grad, self.sources[1].get_result_matrix()).flatten()
         self.sources[1].grad += self.sources[0].get_result_matrix().transpose() @ self.grad
     
     def get_output_dimensions(self) -> Tuple[int, int]:
@@ -520,13 +576,13 @@ class Sum(AbstractOperationNode):
 
     def __init__(self, a: AbstractOperationNode):
         super().__init__(a)
-        assert(a.get_output_dimensions()[1] == 1)
+        # assert(a.get_output_dimensions()[1] == 1)
 
     def inner_run(self, inputs) -> arrayf: # returns a vector of length output length
         return sum(self.sources[0].result)
 
     def propagate_grad(self): # assume that grad is completely setup
-        self.sources[0].grad += sum([self.grad for _ in self.sources])
+        self.sources[0].grad += self.grad
     
     def get_output_dimensions(self) -> Tuple[int, int]:
         return (1, 1)
@@ -534,6 +590,82 @@ class Sum(AbstractOperationNode):
     def __str__(self) -> str:
         return "sum(" + str(self.sources[0]) + ")"
 
+# matrix-vector multiplication
+class Convolve(AbstractOperationNode):
+    kernel_size: Tuple[int, int]
+    strides: Tuple[int, int]
+    output_size: Tuple[int, int]
+    convolution_shape: Tuple[int, int, int, int]
+
+    last_strided = None
+
+    def __init__(self, image: AbstractOperationNode, stencil: AbstractOperationNode, strides: Tuple[int, int]):
+        super().__init__(image, stencil)
+        self.strides = strides
+        input_size = image.get_output_dimensions()
+        self.kernel_size = stencil.get_output_dimensions()
+        self.output_size = ((input_size[0] - self.kernel_size[0]) // strides[0] + 1, (input_size[1] - self.kernel_size[1]) // strides[1] + 1)
+        self.convolution_shape = (self.output_size[0], self.output_size[1], self.kernel_size[0], self.kernel_size[1])
+
+
+    def inner_run(self, inputs) -> arrayf: # returns a vector of length output length
+        image = self.sources[0].get_result_matrix()
+        stencil = self.sources[1].get_result_matrix()
+        image_strides = (image.strides[0] * self.strides[0], image.strides[1] * self.strides[1], image.strides[0] * 1, image.strides[1] * 1)
+        self.last_strided = as_strided(image, shape=self.convolution_shape, strides=image_strides)
+        result = np.einsum('ijkl,kl->ij', self.last_strided, stencil)
+        return result.flatten()
+
+    def propagate_grad(self): # assume that grad is completely setup
+        grad_reshaped = self.get_grad_matrix()
+
+        # image grad
+        image_grad = self.sources[0].get_grad_matrix()
+        stencil = self.sources[1].get_result_matrix()
+        grad_strides = (image_grad.strides[0] * self.strides[0], image_grad.strides[1] * self.strides[1], image_grad.strides[0] * 1, image_grad.strides[1] * 1)
+        grad_strided = as_strided(image_grad, shape=self.convolution_shape, strides=grad_strides, writeable=True)
+        
+        change = np.einsum('ij,kl->ijkl', grad_reshaped, stencil)
+        batches_needed_x = (self.kernel_size[0] - self.strides[0] + 1)
+        batches_needed_y = (self.kernel_size[1] - self.strides[1] + 1)
+        for x in range(batches_needed_x):
+            for y in range(batches_needed_y):
+                grad_strided[x::batches_needed_x, y::batches_needed_y] += change[x::batches_needed_x, y::batches_needed_y]
+        # doesn't work because of concurrency issues
+        # grad_strided += np.einsum('ij,kl->ijkl', grad_reshaped, stencil)
+
+        # stencil grad
+        self.sources[1].grad += (np.einsum('ijkl,ij->kl', self.last_strided, grad_reshaped)).flatten()
+    
+    def get_output_dimensions(self) -> Tuple[int, int]:
+        return self.output_size
+
+    def __str__(self) -> str:
+        return "(" + str(self.sources[0]) + " * " + str(self.sources[1]) + ")"
+
+class Reshape(AbstractOperationNode):
+
+    output_size: Tuple[int, int]
+
+    def __init__(self, a: AbstractOperationNode, output_size: Tuple[int, int]):
+        super().__init__(a)
+        self.output_size = output_size
+        if (np.prod(a.get_output_dimensions()) != np.prod(output_size)):
+            print("expected area: " + str(np.prod(a.get_output_dimensions())))
+            print("desired area: " + str(np.prod(output_size)))
+            assert(np.prod(a.get_output_dimensions()) == np.prod(output_size))
+
+    def inner_run(self, inputs) -> arrayf: # returns a vector of length output length
+        return self.sources[0].result
+    
+    def propagate_grad(self): # assume that grad is completely setup
+        self.sources[0].grad += self.grad
+    
+    def get_output_dimensions(self) -> Tuple[int, int]:
+        return self.output_size
+
+    def __str__(self) -> str:
+        return "{" + str(self.sources[0]) + "}"
 
 class AbstractNetworkLayer:
 
@@ -549,10 +681,32 @@ class ActivationFunction(Enum):
     TANH = 0
     SIGMOID = 1
     RELU = 2
+    NONE = 3
+
+def apply_activation_function(activation_type: ActivationFunction, inputs: Tape) -> Tape:
+    if activation_type == ActivationFunction.TANH:
+        return inputs.tanh()
+    elif activation_type == ActivationFunction.SIGMOID:
+        return inputs.sigmoid()
+    elif activation_type == ActivationFunction.RELU:
+        return inputs.relu()
+    else:
+        return inputs
 
 class InitializationType(Enum):
     NORMAL = 0
     UNIFORM = 1
+    ZEROES = 2
+
+def get_initialization_variables(init_type: InitializationType, parameter_count: int) -> np.ndarray:
+    if init_type == InitializationType.UNIFORM:
+        return np.random.rand(parameter_count)
+    elif init_type == InitializationType.NORMAL:
+        return np.random.randn(parameter_count)
+    elif init_type == InitializationType.ZEROES:
+        return np.zeros(parameter_count)
+    else:
+        return np.zeros(parameter_count)
 
 class SimpleLayer(AbstractNetworkLayer):
 
@@ -560,7 +714,7 @@ class SimpleLayer(AbstractNetworkLayer):
     activation_type: ActivationFunction
     init_type: InitializationType
 
-    def __init__(self, output_vars: int, activation_type: ActivationFunction = ActivationFunction.TANH, init_type: InitializationType = InitializationType.UNIFORM) -> None:
+    def __init__(self, output_vars: int, activation_type: ActivationFunction = ActivationFunction.TANH, init_type: InitializationType = InitializationType.NORMAL) -> None:
         super().__init__()
         self.output_vars = output_vars
         self.activation_type = activation_type
@@ -568,31 +722,62 @@ class SimpleLayer(AbstractNetworkLayer):
 
     def forwards(self, inputs: Tape) -> Tape:
 
-        new_weights = Tape.Var([self.output_vars, len(inputs)])
-        new_biases = Tape.Var([self.output_vars, 1])
+        weights = Tape.Var([self.output_vars, len(inputs)])
+        biases = Tape.Var([self.output_vars, 1])
 
-        ret: Tape = new_weights @ inputs + new_biases
-
-        if self.activation_type == ActivationFunction.TANH:
-            ret = ret.tanh()
-        elif self.activation_type == ActivationFunction.SIGMOID:
-            ret = ret.sigmoid()
-        elif self.activation_type == ActivationFunction.RELU:
-            ret = ret.relu()
-
-        return ret
+        return apply_activation_function(self.activation_type, weights @ inputs + biases)
     
     def initialize(self, parameter_count: int) -> List[float]:
-        if self.init_type == InitializationType.UNIFORM:
-            return np.random.rand(parameter_count)
-        else:
-            return np.random.randn(parameter_count)
+        return get_initialization_variables(self.init_type, parameter_count)
+
+
+class ConvolutionLayer(AbstractNetworkLayer):
+
+    image_shape: Tuple[int, int]
+    stencil_shape: Tuple[int, int]
+    strides: Tuple[int, int]
+    activation_type: ActivationFunction
+    init_type: InitializationType
+
+    def __init__(self, image_shape: Tuple[int, int], stencil_shape: Tuple[int, int], strides: Tuple[int, int], activation_type: ActivationFunction = None, init_type: InitializationType = InitializationType.NORMAL) -> None:
+        super().__init__()
+        self.image_shape = image_shape
+        self.stencil_shape = stencil_shape
+        self.strides = strides
+        self.activation_type = activation_type
+        self.init_type = init_type
+
+    def forwards(self, inputs: Tape) -> Tape:
+
+        stencil = Tape.Var([self.stencil_shape[0], self.stencil_shape[1]])
+
+        convolved = inputs.convolve(stencil, self.strides)
+        return apply_activation_function(self.activation_type, convolved)
+    
+    def initialize(self, parameter_count: int) -> List[float]:
+        return get_initialization_variables(self.init_type, parameter_count)
+
+class ReshapeLayer(AbstractNetworkLayer):
+    
+    output_size: Tuple[int, int]
+
+    def __init__(self, output_size: Tuple[int, int]):
+        super().__init__()
+        self.output_size = output_size
+
+    def forwards(self, inputs: Tape) -> Tape:
+        return inputs.reshape(self.output_size)
+    
+    def initialize(self, parameter_count: int) -> List[float]:
+        return []
 
 class NeuralNetwork:
 
-    input_count: int
+    input_size: Tuple[int, int]
     layers: List[AbstractNetworkLayer]
     learning_rate: float
+    weight_decay_rate: float
+    weight_decay_exp: float
 
     layer_parameter_sizes: List[int]
     total_parameters: int
@@ -605,17 +790,19 @@ class NeuralNetwork:
 
     current_parameters = []
 
-    def __init__(self, input_count: int, layers: List[AbstractNetworkLayer], learning_rate: float) -> None:
-        self.input_count = input_count
+    def __init__(self, input_size: Tuple[int, int], layers: List[AbstractNetworkLayer], learning_rate: float, weight_decay_rate: float = 0, weight_decay_exp: float = 1) -> None:
+        self.input_size = input_size
         self.layers = layers
         self.learning_rate = learning_rate
+        self.weight_decay_rate = weight_decay_rate
+        self.weight_decay_exp = weight_decay_exp
 
         # this is jank, we should find a better way of doing this
         Tape.VARIABLE_INDICES = 0
         old_variable_indices = 0
         self.layer_parameter_sizes = []
 
-        tape = Tape.Const(np.zeros(self.input_count))
+        tape = Tape.Const(np.zeros(self.input_size))
         self.input_layer = tape
 
         for layer in self.layers:
@@ -631,6 +818,9 @@ class NeuralNetwork:
         self.expected_outputs = Tape.Const(np.zeros(self.output_count))
         self.cost = (tape - self.expected_outputs).square().sum().scale(0.5)
 
+        if self.weight_decay_rate != 0:
+            self.cost = self.cost + self.cost.sum_all_variables(lambda x: x**weight_decay_exp).sum().scale(self.weight_decay_rate / self.weight_decay_exp)
+
         self.reset_parameters()
 
     def reset_parameters(self):
@@ -643,6 +833,11 @@ class NeuralNetwork:
     
     def set_expected_outputs(self, expected_outputs: arrayf):
         self.expected_outputs.output_node.val = expected_outputs
+
+    # sanity check
+    def check(self, inputs: arrayf):
+        self.set_inputs(inputs)
+        self.cost.check(self.current_parameters)
 
     def forwards(self, inputs: arrayf) -> arrayf:
         self.set_inputs(inputs)
